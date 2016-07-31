a935703a6471c3f5ef7cc10d4e8eb3affe69a687
Diego-F-Aguirre <diego3884@gmail.com>
2016-07-31 15:12:34 -0600

    initial commit
    The starting point for students

--- ChuckNorrisJokeGenerator/JokeViewController.swift
+++ ChuckNorrisJokeGenerator/JokeViewController.swift
@@ -10,34 +10,6 @@ import UIKit
 
 class JokeViewController: UIViewController {
     
-    @IBOutlet weak var jokeImageView: UIImageView!
     @IBOutlet weak var jokeTextView: UITextView!
     
-    let jokeController = JokeController()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
-    @IBAction func chuckMeButtonPressed(sender: AnyObject) {
-        JokeController.fetchJoke { (joke) in
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                if let joke = joke {
-                    self.jokeTextView.text = joke.joke
-                }
-            })
-        }
-        
-        MemeController.fetchMeme { (memes) in
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                
-                
-                ImageController.imageForURL(memes.randomIndex().imageEndpoint, completion: { (image) in
-                    guard let image = image else { return }
-                    self.jokeImageView.image = image
-                })
-            })
-        }
-    }
 }
